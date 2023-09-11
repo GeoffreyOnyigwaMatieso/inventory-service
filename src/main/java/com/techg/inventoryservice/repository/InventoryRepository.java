641afd6ce66227ead4a27d3bdaaa56f5594817b2
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2023-09-11 11:32:46 +0300

    Added findBySkuCodeIn method in InventoryRepository

--- src/main/java/com/techg/inventoryservice/repository/InventoryRepository.java
+++ src/main/java/com/techg/inventoryservice/repository/InventoryRepository.java
@@ -3,8 +3,11 @@ package com.techg.inventoryservice.repository;
 import com.techg.inventoryservice.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface InventoryRepository extends JpaRepository<Inventory,Long> {
-    Optional<Inventory> findBySkuCode(String  skuCode);
+//    Optional<Inventory> findBySkuCode(String  skuCode);
+
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
 }
