21f72dccf07623d646f83775e1519e0b54037544
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2023-09-11 11:31:06 +0300

    InventoryResponse done

--- src/main/java/com/techg/inventoryservice/dto/InventoryResponse.java
+++ src/main/java/com/techg/inventoryservice/dto/InventoryResponse.java
@@ -1,2 +1,17 @@
-package com.techg.inventoryservice.dto;public class InventoryResponse {
+package com.techg.inventoryservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class InventoryResponse {
+    private String skuCode;
+    private boolean isInStock;
 }
