6c30c56af2bfb6d986991354adbadca7b45c975a
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2023-09-11 11:29:46 +0300

    inventoryController code refactored done

--- src/main/java/com/techg/inventoryservice/controller/InventoryController.java
+++ src/main/java/com/techg/inventoryservice/controller/InventoryController.java
@@ -1,8 +1,11 @@
 package com.techg.inventoryservice.controller;
 
+import com.techg.inventoryservice.dto.InventoryResponse;
 import com.techg.inventoryservice.service.InventoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -14,9 +17,13 @@ public class InventoryController {
         this.inventoryService = inventoryService;
     }
 
-    @GetMapping("/{sku-code}")
+    //http://localhost:8082/api/inventory/iphone-13
+    //http://localhost:8082/api/inventory/iphone-13,iphone13-red
+    /*Displaying as request parameter*/
+    //http://localhost:8082/api/inventory?sku-code=iphone-13&sku-code=iphone13-red
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public  boolean isInStock(@PathVariable("sku-code") String skuCode){
+    public  List<InventoryResponse> isInStock(@RequestParam List<String>  skuCode){
         return inventoryService.isInStock(skuCode);
     }
 }
