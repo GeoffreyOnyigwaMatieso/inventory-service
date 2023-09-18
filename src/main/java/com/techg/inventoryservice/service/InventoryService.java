2f2b5c9b724e5ce84291ff2a267e608897f7b397
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2023-09-18 15:08:50 +0300

    Added spring-cloud-starter-sleuth and zipkin dependencies

--- src/main/java/com/techg/inventoryservice/service/InventoryService.java
+++ src/main/java/com/techg/inventoryservice/service/InventoryService.java
@@ -2,6 +2,9 @@ package com.techg.inventoryservice.service;
 
 import com.techg.inventoryservice.dto.InventoryResponse;
 import com.techg.inventoryservice.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,6 +15,7 @@ import static com.techg.inventoryservice.dto.InventoryResponse.*;
 
 @Service
 //@RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
     //lets inject our inventory repo here
     private final InventoryRepository inventoryRepository;
@@ -20,7 +24,12 @@ public class InventoryService {
         this.inventoryRepository = inventoryRepository;
     }
     @Transactional(readOnly = true)
+    @SneakyThrows
     public  List<InventoryResponse> isInStock(List<String> skuCode){
+        log.info("Wait Started");
+        Thread.sleep(10000);
+        log.info("Wait Ended");
+
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 //mapping the inventory object to the inventory respond object
                 .map(inventory ->
